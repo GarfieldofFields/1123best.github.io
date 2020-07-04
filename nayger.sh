2a66cd25ac0a88ae4c6b4116e07112d8b7257b4f
Paper <37962225+mrpapersonic@users.noreply.github.com>
2020-07-04 11:45:13 -0400

    Update nayger.sh

--- nayger.sh
+++ nayger.sh
@@ -7,12 +7,12 @@ sudo echo "blacklist bcma" >> /etc/modprobe.d/blacklist.conf
 sudo echo "wl" >> /etc/modules
 sudo touch /etc/modprobe.d/wl.conf
 sudo echo "alias wlan0 wl" >> /etc/modprobe.d/wl.conf
-cd /lib/modules/3.4.0/kernel/drivers/net/wireless/
+cd /lib/modules/4.14.174/kernel/drivers/net/wireless/
 wget http://chromium.arnoldthebat.co.uk/files/fw/lib/modules/3.4.0/kernel/drivers/net/wireless/wl.ko
-sudo insmod /lib/modules/3.4.0/kernel/net/wireless/cfg80211.ko
-sudo insmod /lib/modules/3.4.0/kernel/net/wireless/lib80211.ko
-sudo insmod /lib/modules/3.4.0/kernel/net/wireless/lib80211_crypt_ccmp.ko
-sudo insmod /lib/modules/3.4.0/kernel/net/wireless/lib80211_crypt_tkip.ko
-sudo insmod /lib/modules/3.4.0/kernel/net/wireless/lib80211_crypt_wep.ko
-sudo insmod /lib/modules/3.4.0/kernel/drivers/net/wireless/wl.ko
+sudo insmod /lib/modules/4.14.174/kernel/net/wireless/cfg80211.ko
+sudo insmod /lib/modules/4.14.174/kernel/net/wireless/lib80211.ko
+sudo insmod /lib/modules/4.14.174/kernel/net/wireless/lib80211_crypt_ccmp.ko
+sudo insmod /lib/modules/4.14.174/kernel/net/wireless/lib80211_crypt_tkip.ko
+sudo insmod /lib/modules/4.14.174/kernel/net/wireless/lib80211_crypt_wep.ko
+sudo insmod /lib/modules/4.14.174/kernel/drivers/net/wireless/wl.ko
 sudo depmod -a
